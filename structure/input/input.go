02a7862136235ab4acf1a6366082af7fdac8c567
Matheus Kahlis <kahlis.dev@gmail.com>
2026-01-14 12:16:35 -0300

    refactor: Added comments in structure/input/input.go for better documentation and undertanding

--- structure/input/input.go
+++ structure/input/input.go
@@ -3,16 +3,17 @@ package input
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 type KeyInterest struct {
-	Pressed  bool
-	Released bool
-	Down     bool
-	Up       bool
+	Pressed  bool // The key has been pressed once
+	Released bool // The key has been released once
+	Down     bool // The key is being pressed
+	Up       bool // The key is *NOT* being pressed
 }
 
 type InputContext struct {
 	List map[int32]KeyInterest
 }
 
+// Set a new KeyInterest entry for input context
 func (k *InputContext) Set(rlKey int32) {
 	k.List[rlKey] = KeyInterest{
 		Pressed:  rl.IsKeyPressed(rlKey),
@@ -22,6 +23,7 @@ func (k *InputContext) Set(rlKey int32) {
 	}
 }
 
+// Set a list of rlKeys as KeyInterest into InputContext
 func (k *InputContext) BulkSet(rlKeys []int32) {
 	for _, rlKey := range rlKeys {
 		k.List[rlKey] = KeyInterest{
@@ -33,10 +35,12 @@ func (k *InputContext) BulkSet(rlKeys []int32) {
 	}
 }
 
+// Remove a KeyInterest from InputContext by rlKey
 func (k *InputContext) Delete(rlKey int32) {
 	delete(k.List, rlKey)
 }
 
+// Update InputContext keys mapping, should be called on Run()
 func (k *InputContext) Update() {
 	for key := range k.List {
 		k.List[key] = KeyInterest{
@@ -48,6 +52,8 @@ func (k *InputContext) Update() {
 	}
 }
 
+// Return the KeyInterest by its rlKey if is present in InputContext,
+// if not, return false
 func (k *InputContext) GetKey(rlKey int32) KeyInterest {
 	return k.List[rlKey]
 }
